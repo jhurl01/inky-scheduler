07d01c7c3cf5bd0e96cf01fd5e9b310a520a856b
jhurl01 <185558610+jhurl01@users.noreply.github.com>
2026-04-30 12:29:43 -0400

    sec fix

--- lib/screens/month/month_screen.dart
+++ lib/screens/month/month_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import '../../models/canvas_model.dart';
